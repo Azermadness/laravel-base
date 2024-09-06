d50afea1f097f2493b806e6b3e9d148b74bbc3aa
DreamFright <azermadness@protonmail.com>
2024-09-06 09:22:32 +0200

    laravel upgrade / birds added
    upgraded laravel from v8 to v11, bugfixes related to php and linter, finished adding birds to DB and routes works

---
+++ api-rest-laravel/app/Http/Controllers/BirdController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Bird;
+
+class BirdController extends Controller
+{
+    public function index(Request $request)
+    {
+        return response()->json(Bird::all());
+    }
+
+    public function minsize(Request $request)
+    {
+        $birds = Bird::where('taille_min_cm', '=', 12)
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200);
+    }
+
+    public function minsizeforest(Request $request)
+    {
+
+        $birds = Bird::where('taille_min_cm', '=', 12)
+            ->where('habitat', '=', 'Forêts, jardins')
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+
+    public function firstletterM(Request $request)
+    {
+
+        $birds = Bird::where('nom_commun', 'like', 'M%')
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+
+    public function firstletterMComplex(Request $request)
+    {
+
+        $birds = Bird::where('nom_commun', 'like', 'M%')
+            ->orWhere('nom_scientifique', 'like', 'M%')
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+
+    public function jardins(Request $request)
+    {
+
+        $birds = Bird::where('habitat', 'like', '%jardins%')
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+
+    public function poids(Request $request)
+    {
+
+        $birds = Bird::where('poids_min_g', '<=', 15)
+            ->where('poids_max_g', '>=', 15)
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+
+    public function chiante(Request $request)
+    {
+
+        $birds = Bird::where('couleur', 'like', 'Brun%')
+            ->where('taille_min_cm', '>=', 12)
+            ->where('nom_commun', 'like', 'M%')
+            ->where('poids_min_g', '<=', 15)
+            ->where('poids_max_g', '>=', 15)
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+
+    public function sortalphabet(Request $request)
+    {
+
+        $birds = Bird::orderBy('nom_commun', 'asc')
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $birds
+        ], 200); //code 200 s'est bien passé
+    }
+}
--- api-rest-laravel/routes/api.php
+++ api-rest-laravel/routes/api.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BirdController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/birds', [BirdController::class, 'index']);
+Route::get('/birds/minsize', [BirdController::class, 'minsize']);
+Route::get('/birds/minsizeforest', [BirdController::class, 'minsizeforest']);
+Route::get('/birds/firstletterm', [BirdController::class, 'firstletterM']);
+Route::get('/birds/firstlettermcomplex', [BirdController::class, 'firstletterMcomplex']);
+Route::get('/birds/jardins', [BirdController::class, 'jardins']);
+Route::get('/birds/poids', [BirdController::class, 'poids']);
+Route::get('/birds/chiante', [BirdController::class, 'chiante']);
+Route::get('/birds/sortalphabet', [BirdController::class, 'sortalphabet']);
