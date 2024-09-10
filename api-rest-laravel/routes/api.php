32e39fb8821111c9dba79a7d665a2fba01f1a60a
DreamFright <azermadness@protonmail.com>
2024-09-10 16:57:04 +0200

    encryption of owners function

--- api-rest-laravel/routes/api.php
+++ api-rest-laravel/routes/api.php
@@ -37,3 +37,5 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->get('me', [AuthController::class, 'me']);
+
+Route::middleware('auth:sanctum')->get('owners/{id}/homes', [HomeController::class, 'owner_home']);
